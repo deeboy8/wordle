526f7ff30efa618d715549aecc2a8662ff1efcc5
Demitrus Booker <demitrusw.b@gmail.com>
2025-03-06 20:22:38 -0800

    refactored main() and added in methods to classes as applicable

--- wordle.py
+++ wordle.py
@@ -4,6 +4,8 @@ from typing_extensions import Self
 from enum import Enum
 from pydantic import BaseModel, Field
 
+"""!?//todo*"""
+
 MAX_USER_GUESSES = 6
 MAX_WORD_LEN = 5
 
@@ -19,7 +21,7 @@ class Letter(BaseModel):
     name: str = Field(max_length=1)
     letter_state: LetterState = Field(default=LetterState.unselected) #TODO pydantic validation to ensure LetterState is accurate and not returned to unselected
 
-#!word will be string user passes in from stdin
+#word will be string user passes in from stdin
 #must be converted from a string word to a list of letters
 class Word(BaseModel):
     """Will generate a Word class object"""
@@ -30,6 +32,24 @@ class Word(BaseModel):
     @classmethod
     def create(cls, word_str: str) -> 'Word': 
         return cls(word = [Letter(name=ch) for ch in word_str])
+
+    #validate user_guess length and present in dictionary 
+    def validate_user_guess(self, user_guess: str) -> bool:
+        #will use validate_guss  in dict and validate length
+        pass
+    
+    def validate_guess_in_dictionary(self, user_guess: str) -> bool:
+        #check if word in dictionary of words
+        pass
+
+    def validate_guess_length(self, user_guess: str) -> bool:
+        #check if word in dictionary of words
+        pass
+    
+    #score user_guess
+    def score_user_guess(self):
+        #criteria: is 
+        pass
 
 #create a Board as a list and appends each user word 
 class Board(BaseModel):
@@ -49,58 +69,63 @@ class Player (BaseModel):
     pass 
 
 class Game(BaseModel): 
-    def get_secrect_word():
-        #choose a secrect word randomly
+    #gather basic information about user- name
+    #import txt file with potential words and return a list data class
+    def convert_vocabulary_to_list(self, file_name: str):
         pass
     
+    #randomly choose a secrect word and return chosen word
+    def get_secrect_word(self, list_of_words: list):
+        pass
+    
+    #container for user guess
     def user_guess():
-        #take in user guess and hold in variable
         pass
 
-##############################################
-    #methods below this line will be used after user inputs guess
-    #methods above are to be run to initialize a game object 
-    def validate_guess_in_dictionary(self) -> bool:
-        #check if word in dictionary of words
-        pass
-    
-    #TODO: CREATE METHOD TO SHARE USER SUCCESSFUL OR NOT
-    def score_user_guess(self): #TODO: MOVE TO WORD CLASS
-        #return five pieces of info one per letter
+    def generate_board_object():
+        pass 
+
+    def generate_player_object():
         pass
 
 #just write the structure of the game as pseudocode in the main loop
 def main():  
-    #create an instance of a game 
-        #generate player obj
-            #ask for basic details of the user
-    #open file with list of words
-    #choose secrect word and store in var
-
+    #create an instance of a game
+    game = Game()
+        #ask for basic details of the user
+        #open file with optional words and turn into a list of words called vocabulary
+        #choose secrect word and store in var
+        #generate a board object #! DG mentioned user won't have access to Board by placing here in Game obj. what do you mean specifically?
+        #generate a player obj
+        #generate alphabet class
+    
     #iterate over MAX_USER_GUESSES - GAME LOOP -> 1. once have word, compare against secrect word (updated state of word)
         #user enters guess
-        #validate user guess in dict list of potential words #TODO: have to remove a word from a list if it doesn't meet criteria (BINARY SEARCH)
-        #score user guess
-            #convert user guess into a Word obj #TODO: may be created earlier than inside this loop
-            #create a Board obj #TODO: NOT HERE. DON'T NEED A NEW BOARD FOR EACH GAME. game and board seem very tightly coupled as need one of each. Board may never be seen by rest of implemenation, only used by Game obj
-            #update state of alphabets based on user guess
-#TODO: WHERE DO WE UPDATE THE LETTER WITHIN THE WORD
-            #update player info (ie. guesses) #TODO: DON'T NEED A NEW PLAYER FOR EACH WORD
-            #if ansewr corrects:
-                #output congrats
-            #else
-                #output user guess to terminal 
-                    #highlight colors
-
-    #####################################
-    str_to_word_obj = Word.create('happy')
-    # print(str_to_word_obj.word[0].name)
-    # print(str_to_word_obj)
-    obj = Board()
-    obj.insert_user_guess(str_to_word_obj)
-    str_to_word_obj_2 = Word.create('power')
-    obj.insert_user_guess(str_to_word_obj_2)
-    print(obj)
+            # convert to lowercase
+            #validate if it is five letters long
+                #if not return output length is too long or short
+            #each letter is an ASCII character a-z
+                #if not return output length is too long or short
+            #validate if guess in list of vocabulary list
+        #compare against secret word
+            #user_guess == secrect_word
+                #if correct -> congrats
+        #convert user guess into a Word obj -> create method #converting to Word object at this point as previously needed it to be a string for easy comparioson when validating
+        #score word
+            #criteria: 
+            # is letter in secret word
+                #if not, remove word from vocabulary list -> GREY
+                #update general alphabet state from Alphabet class 
+            #letter in word but not in correct location -> YELLOW
+                # update letter state
+            #letter in word and correct position
+                # update latter state
+        #update player details (ie. guesses) 
+        #if ansewr corrects (aka all letters match secrect word)
+            #output congrats
+        #else
+            #output user guess to terminal 
+                #highlight colors
     
 
 if __name__ == "__main__":
