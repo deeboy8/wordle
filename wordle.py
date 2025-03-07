f5d5c540462ac16596e0fd28a70f4375ac397951
Demitrus Booker <demitrusw.b@gmail.com>
2025-03-06 21:32:45 -0800

    added necessary method signatures to Word class

--- wordle.py
+++ wordle.py
@@ -32,23 +32,51 @@ class Word(BaseModel):
     @classmethod
     def create(cls, word_str: str) -> 'Word': 
         return cls(word = [Letter(name=ch) for ch in word_str])
+    
+    def convert_guess_to_lowercase(self, user_guess: str) -> str:
+        return user_guess.lower()
 
-    #validate user_guess length and present in dictionary 
+    #validate user_guess length and present in dictionary and each char is ASCII a-z
     def validate_user_guess(self, user_guess: str) -> bool:
         #will use validate_guss  in dict and validate length
         pass
     
-    def validate_guess_in_dictionary(self, user_guess: str) -> bool:
+    def validate_guess_in_word_list(self, user_guess: Self) -> bool:
         #check if word in dictionary of words
         pass
 
-    def validate_guess_length(self, user_guess: str) -> bool:
+    def validate_guess_length(self, user_guess: Self) -> bool:
         #check if word in dictionary of words
         pass
     
-    #score user_guess
-    def score_user_guess(self):
-        #criteria: is 
+    # check all chars of Word object are a-z
+    def validate_characters_ASCIII(self, user_guess: Self) -> bool:
+        pass
+    
+    def compare_against_secret_word(self, user_guess: Self) -> bool:
+        pass
+    
+    def update_color_state(self, ch: str, color: str) -> bool:
+        # if color is green, yellow, grey:
+            # change ch.letterstate to color -> USE THAT THING THAT STARTS WITH A 'C' AND GIVES OPTIONS TO CHOOSE        
+        pass
+
+    def remove_word_from_list(self, user_guess: Self) -> bool:
+        # create list of letters to form a string
+        # remove string from list 
+        pass 
+
+    #score user_guess for each letter in guess
+    def score_user_guess(self, secret_word: str, user_guess: Self) -> None:
+        #for loop iterating over each letter
+                # if letter in word and in correct location (index): 
+                    # update state with green
+                # elif letter in word but in wrong location (index):
+                    # change state to yellow
+                # else:
+                    #1. change state color to grey
+                    #2. remove all words from list containing that character
+                    #return false
         pass
 
 #create a Board as a list and appends each user word 
@@ -95,22 +123,23 @@ def main():
         #ask for basic details of the user
         #open file with optional words and turn into a list of words called vocabulary
         #choose secrect word and store in var
-        #generate a board object #! DG mentioned user won't have access to Board by placing here in Game obj. what do you mean specifically?
+        #generate a board object #! DG mentioned user won't have access to Board by placing here in Game obj. what do you mean?
         #generate a player obj
         #generate alphabet class
     
     #iterate over MAX_USER_GUESSES - GAME LOOP -> 1. once have word, compare against secrect word (updated state of word)
         #user enters guess
             # convert to lowercase
-            #validate if it is five letters long
+            # convert to word object -> create()
+            # validate if it is five letters long
                 #if not return output length is too long or short
-            #each letter is an ASCII character a-z
+            # each letter is an ASCII character a-z
                 #if not return output length is too long or short
             #validate if guess in list of vocabulary list
         #compare against secret word
             #user_guess == secrect_word
                 #if correct -> congrats
-        #convert user guess into a Word obj -> create method #converting to Word object at this point as previously needed it to be a string for easy comparioson when validating
+        
         #score word
             #criteria: 
             # is letter in secret word
